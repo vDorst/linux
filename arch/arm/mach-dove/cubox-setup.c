53ab904e270e45e986ebc3a0e886e68189d30142
Rabeeh Khoury <rabeeh@solid-run.com>
2012-06-16 12:37:55 +0300

    Clocks changes -
    1. Added silab 5351 driver as generic driver under drivers/clk/
    2. Modified internal clock gating defines
    
    Signed-off-by: Rabeeh Khoury <rabeeh@solid-run.com>

--- arch/arm/mach-dove/cubox-setup.c
+++ arch/arm/mach-dove/cubox-setup.c
@@ -24,6 +24,8 @@
 #include <linux/spi/flash.h>
 #include <linux/gpio.h>
 #include <linux/leds.h>
+#include <linux/clk/si5351.h>
+#include <linux/clk-private.h>
 #include <video/dovefb.h>
 #include <video/dovefbreg.h>
 #include <media/gpio-ir-recv.h>
@@ -41,6 +43,7 @@ static struct mv643xx_eth_platform_data cubox_ge00_data = {
 static struct mv_sata_platform_data cubox_sata_data = {
 	.n_ports        = 1,
 };
+
 static struct sdhci_dove_platform_data cubox_sdio0_data = {
 	.gpio_cd	= 12,
 };
@@ -49,12 +52,13 @@ static struct sdhci_dove_platform_data cubox_sdio0_data = {
  * GPIO setup
  ****************************************************************************/
 static unsigned int cubox_mpp_list[] __initdata = {
-	MPP1_GPIO1,     /* USB Power Enable */
-	MPP2_GPIO2,     /* USB over-current indication */
-	MPP3_GPIO3,     /* micro button beneath eSata port */
-	MPP12_GPIO12,   /* sdio0 card detect */
-	MPP18_GPIO18,   /* red LED */
-	MPP19_GPIO19,   /* IR sensor */
+	MPP1_GPIO1,		/* USB Power Enable */
+	MPP2_GPIO2,		/* USB over-current indication */
+	MPP3_GPIO3,		/* micro button beneath eSata port */
+	MPP12_GPIO12,		/* sdio0 card detect */
+	MPP13_AD1_I2S_EXT_MCLK,	/* i2s1 external clock input */
+	MPP18_GPIO18,		/* red LED */
+	MPP19_GPIO19,		/* IR sensor */
 #if 0
 	/* Not supported for now - FIXME */
 	MPP27_GPIO27,     /* HDMI interrupt */
@@ -83,7 +87,8 @@ static struct dovefb_mach_info dove_cubox_lcd0_dmi = {
 	.id_gfx			= "GFX Layer 0",
 	.id_ovly		= "Video Layer 0",
 	.clk_src		= MRVL_EXT_CLK1,
-	.clk_name		= "SILAB_CLK0",
+//	.clk_name		= "SILAB_CLK0",
+	.clk_name		= "extclk",
 	.pix_fmt		= PIX_FMT_RGB888PACK,
 	.io_pin_allocation	= IOPAD_DUMB24,
 	.panel_rgb_type		= DUMB24_RGB888_0,
@@ -129,6 +134,93 @@ void __init dove_cubox_clcd_init(void)
 #endif /* CONFIG_FB_DOVE */
 }
 
+/***************************************************************************** 
+ * Cubox external clock generator
+ ****************************************************************************/
+static void cubox_extclk_setup(struct device *clkdev)
+{
+	struct clk *clk, *plla, *pllb;
+	int ret;
+
+	clk = plla = pllb = NULL;
+
+	dev_info(clkdev, "external clock setup : clkdev = %p\n", clkdev);
+
+	if (clkdev == NULL)
+		goto cubox_setup_ext_clocks_err;
+
+	plla = clk_get(clkdev, "plla");
+	if (IS_ERR(plla)) 
+		goto cubox_setup_ext_clocks_err;
+
+	pllb = clk_get(clkdev, "pllb");
+	if (IS_ERR(pllb)) 
+		goto cubox_setup_ext_clocks_err;
+
+	/* clkout0 : pllA master, dovefb extclk */
+	clk = clk_get(clkdev, "clkout0");
+	if (IS_ERR(clk)) 
+		goto cubox_setup_ext_clocks_err;
+	clk->flags |= CLK_SET_RATE_PARENT;
+	ret = clk_set_parent(clk, plla);
+	if (ret) {
+		printk(KERN_ERR "failed to set parent for clkout0 : %d\n",
+		       ret);
+		goto cubox_setup_ext_clocks_err;
+	}
+	clk_add_alias("extclk", "dovefb.0", "clkout0", clkdev);
+	clk_put(clk);
+
+	/* clkout1 : pllA slave, hdmi cec clk */
+	clk = clk_get(clkdev, "clkout1");
+	if (IS_ERR(clk)) 
+		goto cubox_setup_ext_clocks_err;
+	clk_set_parent(clk, plla);
+	clk_add_alias("cec", "tda998x", "clkout1", clkdev);
+	clk_put(clk);
+
+	/* clkout2 : pllB master, i2s1 extclk */
+	clk = clk_get(clkdev, "clkout2");
+	if (IS_ERR(clk)) 
+		goto cubox_setup_ext_clocks_err;
+	clk->flags |= CLK_SET_RATE_PARENT;
+	clk_set_parent(clk, pllb);
+	clk_add_alias("extclk", "kirkwood-i2s.1", "clkout2", clkdev);
+	clk_put(clk);
+
+	clk_put(plla);
+	clk_put(pllb);
+
+	printk(KERN_INFO "external clock setup done\n");
+	return;
+
+cubox_setup_ext_clocks_err:
+	printk(KERN_ERR "unable to setup external clocks\n");
+
+	printk(KERN_ERR ">>> plla = %p, pllb = %p, clk = %p\n",
+	       plla, pllb, clk);
+
+	if (!IS_ERR(plla)) 
+		clk_put(plla);
+	if (!IS_ERR(pllb)) 
+		clk_put(pllb);
+}
+
+static struct si5351_clocks_data cubox_clocks_data = {
+	.fxtal = 25000000,
+	.fclkin = 0,
+	.variant = SI5351_VARIANT_A3,
+	.setup = cubox_extclk_setup,
+};
+
+static struct platform_device cubox_extclk = {
+	.name	= "cubox-extclk",
+	.id	= -1,
+	.dev	= {
+		.platform_data	= &cubox_clocks_data,
+	}
+};
+
 /*****************************************************************************
  * I2C devices:
  *      ALC5630 codec, address 0x
@@ -138,7 +230,8 @@ void __init dove_cubox_clcd_init(void)
  ****************************************************************************/
 static struct i2c_board_info __initdata dove_cubox_i2c_bus0_devs[] = {
 	{
-		I2C_BOARD_INFO("silab5351a", 0x60),
+		I2C_BOARD_INFO("si5351", 0x60),
+		.platform_data = &cubox_clocks_data,
 	},
 #ifdef CONFIG_TDA19988
 	{ /* First CEC that enables 0x70 for HDMI */
@@ -252,7 +345,7 @@ static void __init cubox_init(void)
 	dove_sata_init(&cubox_sata_data);
 	dove_sdio0_init(&cubox_sdio0_data);
 	dove_sdio1_init(NULL);
-	dove_i2s1_init();
+	
 	dove_cubox_clcd_init();
 	dove_vmeta_init();
 	dove_gpu_init();
@@ -261,13 +354,16 @@ static void __init cubox_init(void)
 	dove_uart0_init();
 	dove_uart1_init();
 	dove_i2c_init();
-	platform_device_register(&cubox_spdif);
+	
 	i2c_register_board_info(0, dove_cubox_i2c_bus0_devs,
 				ARRAY_SIZE(dove_cubox_i2c_bus0_devs));
-	spi_register_board_info(cubox_spi_flash_info,
-				ARRAY_SIZE(cubox_spi_flash_info));
+	platform_device_register(&cubox_extclk);
 	platform_device_register(&cubox_leds);
 	platform_device_register(&cubox_ir);
+	platform_device_register(&cubox_spdif);
+	dove_i2s1_init();
+	spi_register_board_info(cubox_spi_flash_info,
+				ARRAY_SIZE(cubox_spi_flash_info));
 }
 
 MACHINE_START(CUBOX, "SolidRun CuBox")
